54118fc5bf355ef35202e662be3a61da7a8f21c9
Alexander <alexkost.f@gmail.com>
2020-06-06 00:33:06 +0300

    Improved site adaptivness

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-const menuBtn = document.querySelector(".mobile-menu")
+const menuBtn = document.querySelector(".mobile-menu");
+const menu = document.querySelector(".menu-sidebar");
+
+menuBtn.addEventListener('click', function(event) {
+    if (menu.style.display === "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+});
